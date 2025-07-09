a7e95a57a399820a3c763146279d3adc27e9981a
George Garcia <garcia.george.b@gmail.com>
2025-07-08 21:59:41 -0700

    Update Nav bar UI

--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -1,6 +1,21 @@
-import { Drawer } from 'expo-router/drawer';
 import { SideMenu } from '@/components/organisms';
+import { Drawer } from 'expo-router/drawer';
 
 export default function DrawerLayout() {
-  return <Drawer drawerContent={(props) => <SideMenu {...props} />} />;
+  return (
+    <Drawer drawerContent={(props) => <SideMenu {...props} />}>
+      <Drawer.Screen
+        name="index"
+        options={{
+          headerShown: true,
+          headerTitle: '',
+          headerStyle: {
+            backgroundColor: '#1d2129',
+          },
+          headerTintColor: '#fff', // For text and icons
+          headerShadowVisible: false, // This removes the line under the header
+        }}
+      />
+    </Drawer>
+  );
 }
